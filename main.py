cb52565a76d77466b703ad5912a31c196c743e7e
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2026-04-17 08:05:38 +0100

    removed hardcoded credentials

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "db")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT", "5432")
 POSTGRES_DB = os.environ.get("POSTGRES_DB", "task_tracker_db")
 POSTGRES_USER = os.environ.get("POSTGRES_USER", "user")
-POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "password")
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+if not POSTGRES_PASSWORD:
+    raise RuntimeError("POSTGRES_PASSWORD environment variable is required")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
